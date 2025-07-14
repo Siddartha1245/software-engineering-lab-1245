5fbac038e1bb86e63a9d3bb8b9240f7fc1cfe4c3
Siddartha1245 <spaloju@gitam.in>
2025-07-14 14:42:56 +0530

    Add files via upload

---
+++ two.py
@@ -0,0 +1,6 @@
+a=float(input("Enter coefficient a:"))
+b=float(input("Enter coefficient b:"))
+c=float(input("Enter coefficient c:"))
+x=float(input("Enter x: "))
+T=a*(x*x)+(b*x)+c
+print(f"{x} {T:.2f}")
