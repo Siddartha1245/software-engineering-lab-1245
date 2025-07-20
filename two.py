25571e525d6b5dc44bd05a268e2fd09e985470fb
Siddartha1245 <spaloju@gitam.in>
2025-07-20 23:21:56 +0530

    Delete two.py

--- two.py
+++
@@ -1,6 +0,0 @@
-a=float(input("Enter coefficient a:"))
-b=float(input("Enter coefficient b:"))
-c=float(input("Enter coefficient c:"))
-x=float(input("Enter x: "))
-T=a*(x*x)+(b*x)+c
-print(f"{x} {T:.2f}")
